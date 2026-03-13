fed56da6e95639854681b72829f2c13bb8bba20d
tanveermurtaza-5265 <tanveermurtaza188rb@gmail.com>
2026-03-13 10:28:17 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace project_3_listbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fruitlistbox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Fruitlistbox
+            // 
+            this.Fruitlistbox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Fruitlistbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fruitlistbox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Fruitlistbox.FormattingEnabled = true;
+            this.Fruitlistbox.ItemHeight = 25;
+            this.Fruitlistbox.Items.AddRange(new object[] {
+            "LYCHEE",
+            "STRAWBERY ",
+            "GUAVA",
+            "GRAPES",
+            "COCONUT",
+            "KIWI",
+            "CHERRY",
+            "PEACH",
+            "PEAR",
+            "WATERMELO"});
+            this.Fruitlistbox.Location = new System.Drawing.Point(36, 131);
+            this.Fruitlistbox.Name = "Fruitlistbox";
+            this.Fruitlistbox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.Fruitlistbox.Size = new System.Drawing.Size(247, 479);
+            this.Fruitlistbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(316, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ADD ITEMS:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(510, 145);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(510, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(394, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ADD ITEMS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(511, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(393, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "COUNT ITEMS";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(511, 355);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(386, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SORT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(510, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(386, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "REMOVE SPECIFIC ITEMS";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(510, 510);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(386, 43);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "CLEAR ALL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(614, 60);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "FRUIT LIST BOX CONROL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 641);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Fruitlistbox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My List Box Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Fruitlistbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_3_listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fruitlistbox.Items.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LIST ITEMS:"+Fruitlistbox.Items.Count.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Fruitlistbox.Sorted = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Fruitlistbox.Items.Remove(Fruitlistbox.SelectedItem);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Fruitlistbox.Items.Clear();
+        }
+    }
+}
